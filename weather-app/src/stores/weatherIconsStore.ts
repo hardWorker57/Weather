fae501c4fe77341802c00208647d8ad225d2ed99
hardWorker57 <99531629+hardWorker57@users.noreply.github.com>
2025-02-18 03:35:14 +0600

    test-icons

--- weather-app/src/stores/weatherIconsStore.ts
+++ weather-app/src/stores/weatherIconsStore.ts
@@ -3,13 +3,13 @@ import {defineStore} from "pinia";
 export const useWeatherIconStore = defineStore("weatherIcons", () => {
   
   const iconsMap = {
-    "Clouds": "/src/assets/images/clouds.gif",
-    "Rain": "/src/assets/images/rain.gif",
-    "Clear": "/src/assets/images/sun.gif",
-    "Storm": "/src/assets/images/storm.gif",
-    "Night": "/src/assets/images/night.gif",
-    "Foggy": "/src/assets/images/Foggy.gif",
-    "Snow": "/src/assets/images/weather.gif"
+    "Clouds": "@/assets/images/clouds.gif",
+    "Rain": "@/assets/images/rain.gif",
+    "Clear": "@/assets/images/sun.gif",
+    "Storm": "@/assets/images/storm.gif",
+    "Night": "@/assets/images/night.gif",
+    "Foggy": "@/assets/images/Foggy.gif",
+    "Snow": "@/assets/images/weather.gif"
   }
   
   return {
